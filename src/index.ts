37319b6eadfff08686ebbfa3045daf67bceae180
cotttpan <cotttpan@gmail.com>
2018-01-30 09:11:12 +0900

    fix: Impluve initial rendering

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs/Observable'
-import { debounceTime, distinctUntilChanged, observeOn } from 'rxjs/operators'
-import { subscribeOn } from 'rxjs/operators/subscribeOn' // https://github.com/ReactiveX/rxjs/issues/2900
+import { concat } from 'rxjs/observable/concat'
+import { take, debounceTime, distinctUntilChanged, observeOn } from 'rxjs/operators'
 import { animationFrame } from 'rxjs/scheduler/animationFrame'
 import { mount } from '@cotto/sinkdom'
 import { flux, isAction } from 'flux-helpers'
@@ -14,13 +14,16 @@ import logger from '@/model/devtools/devtools'
 
 const models = [timer, timerTitle, logger]
 const store = flux(models, { wildcard: true })
+const state$ = concat(
+    store.state$.pipe(take(1)), // 最初の1回は同期的に取得する
+    store.state$.pipe(debounceTime(1)), // それ以降は同期的更新通知を1つにまとめるためにdebounceする
+)
 
 //
 // ─── VIEW ───────────────────────────────────────────────────────────────────────
 //
 import App from '@/view/app'
-const view = App(store.state$.pipe(debounceTime(1)))
-
+const view = App(state$)
 //
 // ─── RENDER ─────────────────────────────────────────────────────────────────────
 //
@@ -33,7 +36,6 @@ const handleEventWith = (listener: (ev: Event) => any) => (ev: Event) => {
 const proxy = (next$: Observable<any>) => next$.pipe(
     distinctUntilChanged(),
     observeOn(animationFrame),
-    subscribeOn(animationFrame),
 )
 
 /* mount */
